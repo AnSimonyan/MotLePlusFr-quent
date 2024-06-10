c52092947fed0dcffaafc798ae4df68fd08cf172
AnSimonyan <anna.simona1976@gmail.com>
2024-06-10 15:08:17 +0300

    Добавьте файлы проекта.

---
+++ MotLePlusFréquent/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MotLePlusFréquent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
